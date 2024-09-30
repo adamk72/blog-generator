822d5e646ec77c381c4f32ea185a7213f0329575
Adam Kecskes <adam@kecskes.net>
2024-09-29 20:31:05 -0500

    add more structure with newtype

--- Main.hs
+++ Main.hs
@@ -1,6 +1,17 @@
 main :: IO ()
 main = putStrLn myhtml
 
+newtype Html = Html String
+newtype Structure = Structure String
+
+getStructureString :: Structure -> String
+getStructureString (Structure str) = str
+{- or:
+getStructureString struct =
+  case struct of
+    Structure str -> str
+-}
+
 myhtml :: String
 myhtml =
   makeHtml
@@ -9,6 +20,19 @@ myhtml =
 
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+render :: Html -> String
+render html =
+  case html of
+    Html str -> str
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) =
+  Structure (a <> b)
+{- my attempt:
+append_ :: Structure -> Structure -> Structure
+append_ s1 s2 = Structure (getStructureString s1 <> getStructureString s1)
+-}
 
 html_ :: String -> String
 html_ = el "html"
@@ -19,18 +43,21 @@ body_ :: String -> String
 body_ = el "body"
 
 title_ :: String -> String
-title_ = el "title" 
+title_ = el "title"
 
 head_ :: String -> String
-head_ = el "head" 
+head_ = el "head"
+
+p_ :: String -> Structure
+p_ = Structure . el "p"
+{- previously: 
 p_ :: String -> String
-p_ = el "p" 
+p_ = el "p"
+-}
 
 h1_ :: String -> String
-h1_ = el "h1" 
-
+h1_ = el "h1"
 
 el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-

10350e2dcb6f896a0309625de8e2b7cc1cda9e24
Adam Kecskes <adam@kecskes.net>
2024-09-29 20:36:27 -0500

    refactored based on tutorial

--- Main.hs
+++ Main.hs
@@ -1,62 +1,50 @@
 main :: IO ()
-main = putStrLn myhtml
+main = putStrLn (render myhtml)
+
+myhtml :: Html
+myhtml =
+  html_
+    "My title"
+    ( append_
+      (h1_ "Heading")
+      ( append_
+        (p_ "Paragraph #1")
+        (p_ "Paragraph #2")
+      )
+    )
 
 newtype Html = Html String
 newtype Structure = Structure String
+type Title = String
+
+append_ :: Structure -> Structure -> Structure
+append_ c1 c2 =
+  Structure (getStructureString c1 <> getStructureString c2)
 
 getStructureString :: Structure -> String
-getStructureString (Structure str) = str
-{- or:
-getStructureString struct =
-  case struct of
+getStructureString content =
+  case content of
     Structure str -> str
--}
-
-myhtml :: String
-myhtml =
-  makeHtml
-    "Hello title"
-    (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
-
-makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 render :: Html -> String
 render html =
   case html of
     Html str -> str
 
-append_ :: Structure -> Structure -> Structure
-append_ (Structure a) (Structure b) =
-  Structure (a <> b)
-{- my attempt:
-append_ :: Structure -> Structure -> Structure
-append_ s1 s2 = Structure (getStructureString s1 <> getStructureString s1)
--}
-
-html_ :: String -> String
-html_ = el "html"
-{- This works because 'html_' becomes 'el "html"' and so something like 'html_ "foo"' becomes 'el "html" "foo"' which works for 'el' because it takes a String -> String.
--}
-
-body_ :: String -> String
-body_ = el "body"
-
-title_ :: String -> String
-title_ = el "title"
-
-head_ :: String -> String
-head_ = el "head"
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el "html"
+      ( el "head" (el "title" title)
+        <> el "body" (getStructureString content)
+      )
+    )
 
 p_ :: String -> Structure
 p_ = Structure . el "p"
-{- previously: 
-p_ :: String -> String
-p_ = el "p"
--}
 
-h1_ :: String -> String
-h1_ = el "h1"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el tag content =

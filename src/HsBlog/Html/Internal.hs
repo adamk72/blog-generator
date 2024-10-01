fd5d6abbc2c3e9e9a0f0965dd757b9fe8eb3208e
Adam Kecskes <adam@kecskes.net>
2024-09-30 20:25:00 -0500

    add buildIndex

--- src/HsBlog/Html/Internal.hs
+++ src/HsBlog/Html/Internal.hs
@@ -125,3 +125,26 @@ escape =
         _ -> [c]
   in
     concat . map escapeChar
+
+buildIndex :: [(FilePath, Markup.Document)] -> Html.Html
+buildIndex files =
+  let
+    previews =
+      map
+        ( \(file, doc) ->
+          case doc of
+            Markup.Heading 1 heading : article ->
+              Html.h_ 3 (Html.link_ file (Html.txt_ heading))
+                <> foldMap convertStructure (take 3 article)
+                <> Html.p_ (Html.link_ file (Html.txt_ "..."))
+            _ ->
+              Html.h_ 3 (Html.link_ file (Html.txt_ file))
+        )
+        files
+  in
+    Html.html_
+      "Blog"
+      ( Html.h_ 1 (Html.link_ "index.html" (Html.txt_ "Blog"))
+        <> Html.h_ 2 (Html.txt_ "Posts")
+        <> mconcat previews
+      )
